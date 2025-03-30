f2e350da521e77e6a9e6c98fdc0ff161eb63f701
orlov4919 <drorlov@stud.etu.ru>
2025-03-31 00:55:47 +0300

    удалена не нужная папка с файловым конфигом

--- internal/infrastructure/scrapperhandlers/chatHandler.go
+++ internal/infrastructure/scrapperhandlers/chatHandler.go
@@ -2,6 +2,7 @@ package scrapperhandlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"linkTraccer/internal/domain/dto"
 	"log/slog"
@@ -10,8 +11,13 @@ import (
 )
 
 const (
-	contentType = "Content-Type"
-	jsonType    = "application/json"
+	contentType      = "Content-Type"
+	jsonType         = "application/json"
+	wrongID          = "id не соответствует числу"
+	negativeID       = "полученное id < 0, должно быть id >=0"
+	errId            = "id error"
+	idRegistrated    = "id уже зарегистрирован"
+	idNotRegistrated = "id не зарегистрирован"
 )
 
 type ChatHandler struct {
@@ -27,29 +33,16 @@ func NewChatHandler(repo UserRepo, log *slog.Logger) *ChatHandler {
 }
 
 func (c *ChatHandler) HandleChatChanges(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set(contentType, jsonType)
-
 	userID, err := strconv.ParseInt(mux.Vars(r)["id"], 10, 64)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-
-		err := json.NewEncoder(w).Encode(dto.NewAPIErrResponse("неправильный формат данных в теле", err.Error(), []string{}))
-
-		if err != nil {
-			c.log.Debug("ошибка при энкодинге тела", "err", err.Error())
-		}
+		c.writeAPIErrToResponse(w, wrongID, err.Error(), http.StatusBadRequest)
 
 		return
 	}
 
 	if userID < 0 {
-		w.WriteHeader(http.StatusBadRequest)
-		err := json.NewEncoder(w).Encode(dto.NewAPIErrResponse("id error", "id < 0", []string{}))
-
-		if err != nil {
-			c.log.Debug("ошибка при формировании json ответа", "err", err.Error())
-		}
+		c.writeAPIErrToResponse(w, errId, negativeID, http.StatusBadRequest)
 
 		return
 	}
@@ -59,56 +52,71 @@ func (c *ChatHandler) HandleChatChanges(w http.ResponseWriter, r *http.Request) 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 
-		c.log.Info("ошибка при проверке пользователя в БД", "err", err.Error())
+		c.log.Info(
+			fmt.Sprintf("обработка запроса %s закончилась ошибкой, при проверке пользователя в БД", r.URL.Path),
+			"err", err.Error())
 
 		return
 	}
 
 	switch r.Method {
 	case http.MethodPost:
-
-		if userExist {
-			w.WriteHeader(http.StatusBadRequest)
-
-			err := json.NewEncoder(w).Encode(dto.NewAPIErrResponse("id error", "id exist", []string{}))
-
-			if err != nil {
-				c.log.Debug("ошибка при формировании json ответа", "err", err.Error())
-			}
-
-			return
-		}
-
-		if err := c.userRepo.RegUser(userID); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-
-			c.log.Info("ошибка при регистрации пользователя", "err", err.Error())
-		} else {
-			w.WriteHeader(http.StatusOK)
-		}
+		c.PostHandler(w, userID, userExist)
 
 	case http.MethodDelete:
+		c.DeleteHandler(w, userID, userExist)
 
-		if !userExist {
-			w.WriteHeader(http.StatusNotFound)
-
-			err := json.NewEncoder(w).Encode(dto.NewAPIErrResponse("id error", "нет чата", []string{}))
-
-			if err != nil {
-				c.log.Debug("ошибка при формировании json ответа", "err", err.Error())
-			}
-
-			return
-		}
-
-		if err := c.userRepo.DeleteUser(userID); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-
-			c.log.Info("ошибка при регистрации пользователя", "err", err.Error())
-		} else {
-			w.WriteHeader(http.StatusOK)
-		}
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func (c *ChatHandler) PostHandler(w http.ResponseWriter, userID int64, userExist bool) {
+
+	if userExist {
+		c.writeAPIErrToResponse(w, errId, idRegistrated, http.StatusBadRequest)
+
+		return
+	}
+
+	if err := c.userRepo.RegUser(userID); err != nil {
+		w.Header().Set(contentType, jsonType)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		c.log.Info("ошибка при регистрации пользователя", "err", err.Error())
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (c *ChatHandler) DeleteHandler(w http.ResponseWriter, userID int64, userExist bool) {
+
+	if !userExist {
+		c.writeAPIErrToResponse(w, errId, idNotRegistrated, http.StatusNotFound)
+
+		return
+	}
+
+	if err := c.userRepo.DeleteUser(userID); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		c.log.Info("ошибка при регистрации пользователя", "err", err.Error())
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (c *ChatHandler) writeAPIErrToResponse(w http.ResponseWriter, errName, errDescription string, statusCode int) {
+	w.Header().Set(contentType, jsonType)
+	w.WriteHeader(statusCode)
+
+	err := json.NewEncoder(w).Encode(dto.NewAPIErrResponse(errName, errDescription, []string{}))
+
+	if err != nil {
+		c.log.Debug("ошибка при формировании JSON APIErrResponse", "err", err.Error())
 	}
 }
